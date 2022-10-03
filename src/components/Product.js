f5b8880a7728b488ec0744b78a709859e3f6dbeb
hamsa <hafieekhan@gmail.com>
2022-10-04 00:26:20 +0300

    "Created NavBar & Product Component"

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Product = () => {
-  return <div>Product</div>;
+  return (
+    <div>
+      <h1>Product</h1> Product
+    </div>
+  );
 };
 
 export default Product;
