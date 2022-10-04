8c66447efaa1feb82e94403eb29335abe0df4088
hamsa <hafieekhan@gmail.com>
2022-10-04 20:35:54 +0300

    used useEffect to fetch data"

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,44 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ProductsCard from "./ProductsCard";
 import Search from "./Search";
 
-const productListData = [
-  {
-    id: 1,
-    title: "Hooded Jacket with Drawstring",
-    price: "ksh 109.95",
-    description: "Your perfect hood for everyday use and walks in the forest. ",
-    category: "Men's ",
-    image:
-      "https://cdn.shopify.com/s/files/1/0604/9513/4942/products/HoodedJacketwithDrawstring1.jpg?v=1648303697",
-  },
-  {
-    id: 2,
-    title: "Men's",
-    price: "Ksh:22.3",
-    description: "Slim-fitting style, contrast raglan long sleeve.",
-    category: "Men's",
-    image:
-      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-  },
-  {
-    id: 3,
-    title: "Mens Cotton Jacket",
-    price: 55.99,
-    description: "great outerwear jackets for Spring/Autumn/Winter.",
-    category: "Men's",
-    image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-  },
-];
-
 function ProductList() {
+  const [productListData, setProductListData] = useState([]);
+  // const baseUrl = "https://polar-tundra-37099.herokuapp.com/products";
+
+  useEffect(() => {
+    const baseUrl = "https://polar-tundra-37099.herokuapp.com/products";
+    fetch(baseUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setProductListData(data);
+      });
+  }, []);
+
   const allProduct = productListData.map((productObject) => {
     return <ProductsCard key={productObject.id} product={productObject} />;
   });
   return (
     <div>
       <Search />
-      {allProduct}
+      <div className="row">{allProduct}</div>
     </div>
   );
 }
--- src/components/ProductsCard.js
+++ src/components/ProductsCard.js
@@ -1,24 +1,25 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
-import CardGroup from "react-bootstrap/CardGroup";
+// import Card from "react-bootstrap/Card";
+// import CardGroup from "react-bootstrap/CardGroup";
+import "./Card.css";
 
 function ProductsCard({ product }) {
   return (
-    <CardGroup>
-      <Card>
-        <Card.Img variant="top" src={product.image} />
-        <Card.Body>
-          <Card.Title>
-            {product.title} {product.price}
-          </Card.Title>
-          <Card.Text>{product.description}</Card.Text>
-          <Card.Text>{product.category}</Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <button className="button"> Add to Cart</button>
-        </Card.Footer>
-      </Card>
-    </CardGroup>
+    <div className="col">
+      <div className="card" style={{ width: 18 + "rem" }}>
+        <img src={product.image} alt="" className="image" />
+        <div className="card-body"></div>
+        <div className="card__info">
+          <h2>Title:{product.title}</h2>
+          <h4>Price:{product.price}</h4>
+          <h4>Description:{product.description}</h4>
+          <h4>Catgory:{product.category}</h4>
+          <div>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
