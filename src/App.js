18d3758ef28204dee9670e04476539057cc2eb05
hamsa <hafieekhan@gmail.com>
2022-10-05 11:32:40 +0300

    "Updated"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
 import ProductList from "./components/ProductList";
-import { BrowserRouter as Routers, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
     <div className="App">
-      <Routers>
-        <NavBar />
-        <Routes>
-          {/* <Route exact path="/" element={<Home />}></Route> */}
-          <Route exact path="/" element={<ProductList />}></Route>
-        </Routes>
-      </Routers>
+      <NavBar />
+      <Routes>
+        {/* <Route exact path="/" element={<Home />}></Route> */}
+        <Route exact path="/" element={<ProductList />} />
+      </Routes>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 
   document.getElementById("root")
 );
