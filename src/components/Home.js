ead29a5489f17f33ab9bdf8e12491eeeeab0336e
hamsa <hafieekhan@gmail.com>
2022-10-08 14:24:48 +0300

    "Styled Header.js and Home.js"

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,26 +6,26 @@ function Home() {
     <div className="home">
       <div
         id="carouselExampleIndicators"
-        class="carousel slide"
+        className="carousel slide"
         data-ride="carousel"
       >
-        <ol class="carousel-indicators">
+        <ol className="carousel-indicators">
           <li
             data-target="#carouselExampleIndicators"
             data-slide-to="0"
-            class="active"
+            className="active"
           ></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
         </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img
-              class="d-block w-100"
+        <div className="carousel-inner" >
+          <div className="carousel-item active">
+            <img id="images"
+              className="d-block w-100"
               src={require("./men.jpg")}
               alt="First slide"
             />
-            {/* <div class="carousel-caption d-none d-md-block">
+            {/* <div className="carousel-caption d-none d-md-block">
               <h5>My Caption Title (1st Image)</h5>
               <p>
                 The whole caption will only show up if the screen is at least
@@ -33,38 +33,44 @@ function Home() {
               </p>
             </div> */}
           </div>
-          <div class="carousel-item">
+          <div className="carousel-item">
             <img
-              class="d-block w-100"
+              className="d-block w-100"
               src={require("./women.jpg")}
               alt="Second slide"
             />
           </div>
-          <div class="carousel-item">
+          <div className="carousel-item">
             <img
-              class="d-block w-100"
+              className="d-block w-100"
               src={require("./electronics.jpg")}
               alt="Third slide"
             />
           </div>
         </div>
         <a
-          class="carousel-control-prev"
+          className="carousel-control-prev"
           href="#carouselExampleIndicators"
           role="button"
           data-slide="prev"
         >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Previous</span>
         </a>
         <a
-          class="carousel-control-next"
+          className="carousel-control-next"
           href="#carouselExampleIndicators"
           role="button"
           data-slide="next"
         >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Next</span>
         </a>
       </div>
     </div>
